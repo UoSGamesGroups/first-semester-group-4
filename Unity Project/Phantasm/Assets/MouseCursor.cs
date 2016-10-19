41e3a6550a582173afd3318215bcd545c50a8949
Ben Reynolds <benr420@gmail.com>
2016-10-19 17:16:30 +0100

    Created custom cursor script

---
+++ Unity Project/Phantasm/Assets/MouseCursor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseCursor : MonoBehaviour {
+
+	void Start () {
+	    Cursor.visible = false;
+	}
+
+	void Update () {
+
+        var Mouse_Position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Mouse_Position.z = 0;
+	    GetComponent<Transform>().position = Mouse_Position;
+
+	}
+
+}
