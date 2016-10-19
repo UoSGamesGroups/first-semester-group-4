8ee7497532dcd474f1a9b7a2d5ea457cd9d342c5
Ben Reynolds <benr420@gmail.com>
2016-10-19 19:47:41 +0100

    Created camera script that follows a target

---
+++ Unity Project/Phantasm/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform Target;
+    public float Damp_Time;
+    private Vector3 Velocity = Vector3.zero;
+
+    public float Left_Boundary, Right_Boundary;
+
+	void FixedUpdate () {
+
+	    Vector3 Point = GetComponent<Camera>().WorldToViewportPoint(Target.position);
+	    Vector3 Delta = new Vector3(Target.position.x, 0, Target.position.z) -
+	                    GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, Point.z));
+	    Vector3 Destination = GetComponent<Transform>().position + Delta;
+
+	    Vector3 Next_Position = Vector3.SmoothDamp(GetComponent<Transform>().position, Destination,
+	        ref Velocity, Damp_Time);
+
+        if (Next_Position.x >= Left_Boundary && Next_Position.x <= Right_Boundary) {
+            GetComponent<Transform>().position = Next_Position;
+        }
+
+	}
+
+}
