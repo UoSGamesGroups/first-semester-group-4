f4ad33f23abcc7ff13b4b95ea5f061d5bdcca791
Ben Reynolds <benr420@gmail.com>
2016-10-20 13:27:05 +0100

    Added 'Level Changer' item with placeholder art

---
+++ Unity Project/Phantasm/Assets/Scripts/Items/LevelChanger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour {
+
+    public int Scene;
+
+    public void OnTriggerEnter2D(Collider2D _Collision) {
+        if (_Collision.gameObject.tag == "Player") {
+            SceneManager.LoadScene(Scene);
+        }
+    }
+
+}
