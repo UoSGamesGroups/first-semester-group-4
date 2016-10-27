ef9f9c095cf9eeb9c92d4313a3722abb2a13fa10
Ben Reynolds <benr420@gmail.com>
2016-10-27 20:58:40 +0100

    Change mouse cursor to be visible only when required

--- Unity Project/Phantasm/Assets/Scripts/Mouse/MouseCursor.cs
+++ Unity Project/Phantasm/Assets/Scripts/Mouse/MouseCursor.cs
@@ -4,16 +4,24 @@ using System.Collections;
 public class MouseCursor : MonoBehaviour
 {
 
+    public bool showCursor;
+
     private void Start ()
     {
 	    Cursor.visible = false;
-	}
+        GetComponent<SpriteRenderer>().enabled = showCursor;
+    }
 
     private void Update ()
     {
-        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePosition.z = 0;
-	    GetComponent<Transform>().position = mousePosition;
-	}
+        if (showCursor)
+        {
+            var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0;
+            GetComponent<Transform>().position = mousePosition;
+        }
+    }
+
+
 
 }
