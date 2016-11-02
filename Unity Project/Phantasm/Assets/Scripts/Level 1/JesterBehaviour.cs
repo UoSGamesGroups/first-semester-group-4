0b22b245f7b1ca6bba35ce8bb824944e5a4f280f
Ben Reynolds <benr420@gmail.com>
2016-11-02 20:56:58 +0000

    Create Jester protagonist

---
+++ Unity Project/Phantasm/Assets/Scripts/Level 1/JesterBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JesterBehaviour : MonoBehaviour {
+
+    public void flyAway()
+    {
+        StartCoroutine(flyAndDie());
+    }
+
+    private IEnumerator flyAndDie()
+    {
+        yield return new WaitForSeconds(2.0f);
+        GetComponent<Animator>().SetBool("Disappear", true);
+        GetComponent<BoxCollider2D>().isTrigger = true;
+    }
+
+}
---
+++ Unity Project/Phantasm/Assets/Scripts/Level 1/JesterTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JesterTrigger : MonoBehaviour
+{
+
+    public JesterBehaviour jesterBehaviour;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (GameObject.Find("Global State").GetComponent<GlobalState>().getInstance().puzzleData.puzzle1Solved)
+            {
+                jesterBehaviour.flyAway();
+            }
+        }
+    }
+
+
+}
