1c333ab5f43dad6c048bb9296ffdba4d3b9c57f2
Ben Reynolds <benr420@gmail.com>
2016-11-02 20:57:20 +0000

    Create matching game transition

---
+++ Unity Project/Phantasm/Assets/Scripts/Level 1/MatchingGameTransition.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MatchingGameTransition : MonoBehaviour
+{
+
+    public SceneTransitioner sceneTransitioner;
+    public Sprite sprUnsolved, sprSolved;
+    private bool solved;
+
+    private void Start()
+    {
+        solved = GameObject.Find("Global State").GetComponent<GlobalState>().getInstance().puzzleData.puzzle1Solved;
+        GetComponent<SpriteRenderer>().sprite = solved ? sprSolved : sprUnsolved;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && !solved)
+        {
+            StartCoroutine(startGame());
+        }
+    }
+
+    private IEnumerator startGame()
+    {
+        yield return new WaitForSeconds(1.0f);
+        sceneTransitioner.changeScene(2);
+    }
+
+
+}
