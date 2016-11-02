8632988afb86239a59b06f039e0728b5f18c2c1b
Ben Reynolds <benr420@gmail.com>
2016-11-02 20:58:47 +0000

    Fix player movement animations

--- Unity Project/Phantasm/Assets/Scripts/Camera/CameraFollow.cs
+++ Unity Project/Phantasm/Assets/Scripts/Camera/CameraFollow.cs
@@ -13,7 +13,7 @@ public class CameraFollow : MonoBehaviour
     private void FixedUpdate ()
     {
 	    var point = GetComponent<Camera>().WorldToViewportPoint(target.position);
-	    var delta = new Vector3(target.position.x, 0, target.position.z) -
+	    var delta = new Vector3(target.position.x, target.position.y + 2.0f, target.position.z) -
 	                    GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
 	    var destination = GetComponent<Transform>().position + delta;
 
